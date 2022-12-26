64fa83764e96cf8f244a8d584cdbda679d790921
ryhorowitz <ryan.w.horowitz@gmail.com>
2022-12-26 10:36:47 -0500

    commit tree 97718de8721d00be895f9b24077654fd78a30ffc

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const formData = {
+  name: 'ryWitz',
+  email: '123@aol.com'
+}
+
+const configObj = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  body: JSON.stringify(formData)
+}
+
+function submitData() {
+  return fetch('http://localhost:3000/users', configObj)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => {
+      alert('error')
+      console.error(err.message)
+    })
+}
+
+// submitData()
